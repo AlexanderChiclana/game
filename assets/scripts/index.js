181b17a67363bdb02652b6efe92e9f6f67d66645
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2018-06-22 15:26:31 -0400

    working game logic

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,19 +10,143 @@ $(() => {
   // array sections
   let bank = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
 
+  // horizontal arrays
+
+  let firstRow = []
+
+  let secondRow = []
+
+  let thirdRow = []
+
+
+  // vertical arrays
+
+  let firstCol = []
+
+  let secondCol = []
+
+  let thirdCol = []
+
+
+  // diagonal arrays
+
+  let diagTop = []
+
+  let diagBot = []
+
+  // check for wins
+
+
+
+
+
+  const winCheck = function () {
+    // local variables to reassign after evrery invocation
+    const firstRowStr = firstRow.toString()
+    const secondRowStr = secondRow.toString()
+    const thirdRowStr = thirdRow.toString()
+
+    const firstColStr = firstCol.toString()
+    const secondColStr = secondCol.toString()
+    const thirdColStr = thirdCol.toString()
+
+    const diagTopStr = diagTop.toString()
+    const diagBotStr = diagBot.toString()
+
+    const xWins = 'X,X,X'
+    const oWins = 'O,O,O'
+
+    // shorter logic attempt, checking if won condition met
+    if (firstRowStr === xWins || secondRowStr === xWins || thirdRowStr === xWins || firstColStr === xWins ||
+      secondColStr === xWins || thirdColStr === xWins || diagTopStr === xWins || diagBotStr === xWins) {
+
+      console.log('x wins')
+      bank = []
+    } else if (firstRowStr === oWins || secondRowStr === oWins || thirdRowStr === oWins || firstColStr ===
+      oWins || secondColStr === oWins || thirdColStr === oWins || diagTopStr === oWins || diagBotStr === oWins) {
+
+      console.log('O wins')
+      bank = []
+    } else {
+
+      console.log('not yet')
+    }
+  }
+  /*
+      if (firstRowStr === xWins) {
+        console.log('X condition met')
+
+      } else if (firstRowStr === oWins) {
+        console.log('O wins')
+
+      } else if (firstRowStr === xWins) {
+        console.log('x wins')
+
+      } else if (secondRowStr === xWins) {
+        console.log('X wins')
+
+      } else if (secondRowStr === oWins) {
+        console.log('o wins')
+
+      } else if (thirdRowStr === oWins) {
+        console.log('O wins')
+
+      } else if (firsColStr === oWins) {
+        console.log('O wins')
+
+      } else if (secondColStr === xWins) {
+        console.log('X wins')
+
+      } else if (thirdColStr === oWins) {
+        console.log('O wins')
+
+      } else if (diagTopStr === oWins) {
+        console.log('O wins')
+
+      } else if (diagBotStr === xWins) {
+        console.log('X wins')
+
+      } else if (firstRowStr === oWins) {
+        console.log('O wins')
+
+      }
+      }
+      } else {
+        console.log('not yet')
+      }
+
+    }
+
+    */
 
   // click handlers section for each square
   $('#B0').on('click', function () {
     event.preventDefault()
     // code for already placed
     if ($('#B0').is(':empty')) {
-      // code for valid move
 
+      // code for valid move
+      // places move, adds to row/col array, and takes away from bank
       $('#B0').html(bank[0])
-      bank.shift()
+      firstRow[0] = bank[0]
+      firstCol[0] = bank[0]
+      diagTop[0] = bank[0]
+
+      //check for win
+      winCheck()
+
+      console.log(firstRow)
+      console.log(firstCol)
+
+
+      // will need function to check 3 in a row here
+      // rowCheck()
+
+
+
       console.log('sq 0 clicked')
       console.log(bank)
-
+      bank.shift()
       // code for invalid
     } else {
       console.log('full')
@@ -36,6 +160,11 @@ $(() => {
     if ($('#B1').is(':empty')) {
 
       $('#B1').html(bank[0])
+      firstRow[1] = bank[0]
+      secondCol[0] = bank[0]
+
+      winCheck()
+
       bank.shift()
       console.log('sq 1 clicked')
       console.log(bank)
@@ -52,6 +181,13 @@ $(() => {
     if ($('#B2').is(':empty')) {
 
       $('#B2').html(bank[0])
+      firstRow[2] = bank[0]
+      thirdCol[0] = bank[0]
+      diagTop[2] = bank[0]
+
+
+      winCheck()
+
       bank.shift()
       console.log('sq 2 clicked')
 
@@ -67,6 +203,12 @@ $(() => {
     if ($('#B3').is(':empty')) {
 
       $('#B3').html(bank[0])
+      secondRow[0] = bank[0]
+      firstCol[1] = bank[0]
+
+
+      winCheck()
+
       bank.shift()
       console.log('sq 3 clicked')
 
@@ -81,6 +223,14 @@ $(() => {
     if ($('#B4').is(':empty')) {
 
       $('#B4').html(bank[0])
+
+      secondRow[1] = bank[0]
+      secondCol[1] = bank[0]
+      diagBot[1] = bank[0]
+      diagTop[1] = bank[0]
+
+      winCheck()
+
       bank.shift()
       console.log('sq 4 clicked')
 
@@ -95,6 +245,13 @@ $(() => {
     if ($('#B5').is(':empty')) {
 
       $('#B5').html(bank[0])
+
+      secondRow[2] = bank[0]
+      thirdCol[1] = bank[0]
+
+
+      winCheck()
+
       bank.shift()
       console.log('sq 5 clicked')
 
@@ -109,6 +266,13 @@ $(() => {
     if ($('#B6').is(':empty')) {
 
       $('#B6').html(bank[0])
+      thirdRow[0] = bank[0]
+      firstCol[2] = bank[0]
+      diagBot[0] = bank[0]
+
+
+      winCheck()
+
       bank.shift()
       console.log('sq 6 clicked')
 
@@ -124,6 +288,13 @@ $(() => {
     if ($('#B7').is(':empty')) {
 
       $('#B7').html(bank[0])
+
+      thirdRow[1] = bank[0]
+      secondCol[2] = bank[0]
+
+
+      winCheck()
+
       bank.shift()
       console.log('sq 7 clicked')
 
@@ -139,6 +310,13 @@ $(() => {
     if ($('#B8').is(':empty')) {
 
       $('#B8').html(bank[0])
+
+      thirdRow[2] = bank[0]
+      thirdCol[2] = bank[0]
+      diagTop[2] = bank[0]
+
+      winCheck()
+
       bank.shift()
       console.log('sq 8 clicked')
 
