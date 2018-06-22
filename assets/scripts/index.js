28ce096f9b900c30086c3f315a3e866c30f3a935
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2018-06-22 12:16:25 -0400

    working board

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,5 +7,83 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  // array sections
+  let bank = ['X','O','X','O','X','O','X','O','X']
+
+
+  // click handlers section for each square
+  $('#B0').on('click', function () {
+    event.preventDefault()
+
+    $('#B0').html(bank[0])
+    bank.shift()
+    console.log('sq 0 clicked')
+    console.log(bank)
+  })
+
+  $('#B1').on('click', function () {
+    event.preventDefault()
+
+    $('#B1').html(bank[0])
+    bank.shift()
+    console.log('sq 1 clicked')
+    console.log(bank)
+  })
+
+  $('#B2').on('click', function () {
+    event.preventDefault()
+
+    $('#B2').html(bank[0])
+    bank.shift()
+    console.log('sq 2 clicked')
+  })
+
+  $('#B3').on('click', function () {
+    event.preventDefault()
+
+    $('#B3').html(bank[0])
+    bank.shift()
+    console.log('sq 3 clicked')
+  })
+
+  $('#B4').on('click', function () {
+    event.preventDefault()
+
+    $('#B4').html(bank[0])
+    bank.shift()
+    console.log('sq 4 clicked')
+  })
+
+  $('#B5').on('click', function () {
+    event.preventDefault()
+
+    $('#B5').html(bank[0])
+    bank.shift()
+    console.log('sq 5 clicked')
+  })
+
+  $('#B6').on('click', function () {
+    event.preventDefault()
+
+    $('#B6').html(bank[0])
+    bank.shift()
+    console.log('sq 6 clicked')
+  })
+
+  $('#B7').on('click', function () {
+    event.preventDefault()
+
+    $('#B7').html(bank[0])
+    bank.shift()
+    console.log('sq 7 clicked')
+  })
+
+  $('#B8').on('click', function () {
+    event.preventDefault()
+
+    $('#B8').html(bank[0])
+    bank.shift()
+    console.log('sq 8 clicked')
+  })
+//end of handlers
 })
