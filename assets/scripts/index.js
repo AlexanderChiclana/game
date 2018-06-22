cf844c5df628cbede72984be91df736e726abd9d
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2018-06-22 12:40:38 -0400

    space can only contain one move

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,82 +8,143 @@
 
 $(() => {
   // array sections
-  let bank = ['X','O','X','O','X','O','X','O','X']
+  let bank = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
 
 
   // click handlers section for each square
   $('#B0').on('click', function () {
     event.preventDefault()
+    // code for already placed
+    if ($('#B0').is(':empty')) {
+      // code for valid move
 
-    $('#B0').html(bank[0])
-    bank.shift()
-    console.log('sq 0 clicked')
-    console.log(bank)
+      $('#B0').html(bank[0])
+      bank.shift()
+      console.log('sq 0 clicked')
+      console.log(bank)
+
+      // code for invalid
+    } else {
+      console.log('full')
+    }
+
   })
 
   $('#B1').on('click', function () {
     event.preventDefault()
 
-    $('#B1').html(bank[0])
-    bank.shift()
-    console.log('sq 1 clicked')
-    console.log(bank)
+    if ($('#B1').is(':empty')) {
+
+      $('#B1').html(bank[0])
+      bank.shift()
+      console.log('sq 1 clicked')
+      console.log(bank)
+
+    } else {
+      console.log('full')
+    }
   })
 
   $('#B2').on('click', function () {
     event.preventDefault()
 
-    $('#B2').html(bank[0])
-    bank.shift()
-    console.log('sq 2 clicked')
+
+    if ($('#B2').is(':empty')) {
+
+      $('#B2').html(bank[0])
+      bank.shift()
+      console.log('sq 2 clicked')
+
+    } else {
+      console.log('full')
+    }
+
   })
 
   $('#B3').on('click', function () {
     event.preventDefault()
 
-    $('#B3').html(bank[0])
-    bank.shift()
-    console.log('sq 3 clicked')
+    if ($('#B3').is(':empty')) {
+
+      $('#B3').html(bank[0])
+      bank.shift()
+      console.log('sq 3 clicked')
+
+    } else {
+      console.log('full')
+    }
   })
 
   $('#B4').on('click', function () {
     event.preventDefault()
 
-    $('#B4').html(bank[0])
-    bank.shift()
-    console.log('sq 4 clicked')
+    if ($('#B4').is(':empty')) {
+
+      $('#B4').html(bank[0])
+      bank.shift()
+      console.log('sq 4 clicked')
+
+    } else {
+      console.log('full')
+    }
   })
 
   $('#B5').on('click', function () {
     event.preventDefault()
 
-    $('#B5').html(bank[0])
-    bank.shift()
-    console.log('sq 5 clicked')
+    if ($('#B5').is(':empty')) {
+
+      $('#B5').html(bank[0])
+      bank.shift()
+      console.log('sq 5 clicked')
+
+    } else {
+      console.log('full')
+    }
   })
 
   $('#B6').on('click', function () {
     event.preventDefault()
 
-    $('#B6').html(bank[0])
-    bank.shift()
-    console.log('sq 6 clicked')
+    if ($('#B6').is(':empty')) {
+
+      $('#B6').html(bank[0])
+      bank.shift()
+      console.log('sq 6 clicked')
+
+
+    } else {
+      console.log('full')
+    }
   })
 
   $('#B7').on('click', function () {
     event.preventDefault()
 
-    $('#B7').html(bank[0])
-    bank.shift()
-    console.log('sq 7 clicked')
+    if ($('#B7').is(':empty')) {
+
+      $('#B7').html(bank[0])
+      bank.shift()
+      console.log('sq 7 clicked')
+
+    } else {
+      console.log('full')
+    }
+
   })
 
   $('#B8').on('click', function () {
     event.preventDefault()
 
-    $('#B8').html(bank[0])
-    bank.shift()
-    console.log('sq 8 clicked')
+    if ($('#B8').is(':empty')) {
+
+      $('#B8').html(bank[0])
+      bank.shift()
+      console.log('sq 8 clicked')
+
+    } else {
+      console.log('full')
+    }
   })
-//end of handlers
+  //end of handlers
 })
