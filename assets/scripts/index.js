7fec4aa2e217d45ff19093316120ba2b6733123b
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2018-06-25 16:30:10 -0400

    added API functionality

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,8 +5,12 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const authEvents = require('./auth/events.js')
 
 $(() => {
+
+  authEvents.addHandlers()
+
   // array sections
   let bank = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
 
@@ -313,7 +317,7 @@ $(() => {
 
 
 
-  //modal section
+  // modal section, WORK ON LAST ONE
 
   // Get the modal
 
@@ -348,5 +352,15 @@ $(() => {
       modal.style.display = "none";
     }
   }
+
+
+
+
+
+
+
+
+
+
 
 })
