2a3a466553de43a96a1272d0756558e3c7660064
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2018-06-25 10:39:03 -0400

    added modal button

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -37,7 +37,7 @@ $(() => {
   // reset
 
   $('#reset').on('click', function () {
-      // reset arrays
+    // reset arrays
     bank = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
     firstRow = []
     secondRow = []
@@ -310,4 +310,33 @@ $(() => {
     }
   })
   //end of handlers
+
+  //modal section
+
+  // Get the modal
+  var modal = document.getElementById('myModal');
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function () {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
 })
